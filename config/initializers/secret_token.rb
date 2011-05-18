33f8600ad94839d630728c623fc96efcf44aaa94
Ali <rahimi@seamlessinteraction.com>
2011-05-18 11:35:54 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '437182b25ceb9a23b4538ed3d7dc43d3d9a0a26fe8b77cdef30d5f33f8d7786b6fb97f11103391ae8ef257f56d082549a2cf36b5995419fc8253b59e379c5448'
